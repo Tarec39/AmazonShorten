84a305e4fab75b3118cb06f948a0cceea441b56c
Tarek <itcameranian@gmail.com>
2021-10-01 14:23:36 +0900

    [update] Amazonリンクの判定できた。簡単に。#1

--- AmazonShorten.py
+++ AmazonShorten.py
@@ -9,6 +9,11 @@ client = discord.Client()
 async def on_ready():
     print('ログインしました')
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
 
+    if 'https://www.amazon' in message.content:
+        await message.channel.send(" Detected Amazon's Link ")
 client.run(API_TOKEN)
-
